f27c8df9817f82a7edf61ca6a384d31579ccdf0d
doobix <doobix@gmail.com>
2022-07-31 19:26:10 +0000

    Deploying to gh-pages from @ doobix/see-greenhouse@5474655d062332f13608b1e8d88864eae5c36b0c 🚀

--- script.test.js
+++ script.test.js
@@ -17,3 +17,10 @@ test('includes and excludes correct job keywords', () => {
     .filter(script.excludeKeywords);
   expect(filteredJobs.length).toBe(9);
 });
+
+test('sorts job titles correctly', () => {
+  const fakeJobs = [{title: 'zebra'}, {title: 'happy'}, {title: 'avocado'}, {title: 'toast'}];
+  const expectedJobs = [{title: 'avocado'}, {title: 'happy'}, {title: 'toast'}, {title: 'zebra'}];
+  const sortedJobs = fakeJobs.sort(script.sortJobs);
+  expect(sortedJobs).toStrictEqual(expectedJobs);
+});
